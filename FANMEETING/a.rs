1d4928e421612578b3cf223ae30c4bc076bbf794
Hyeon Kim <simnalamburt@gmail.com>
2018-02-28 19:48:55 +0900

    FANMEETING: 풀다맘

--- FANMEETING/a.rs
+++ FANMEETING/a.rs
@@ -11,46 +11,43 @@ fn main() {
 
     let count = read_line().trim_right().parse().unwrap();
     for _ in 0..count {
-        let idol: Vec<_> = read_line().trim_right().chars().map(|e| e == 'F').collect();
-        let fans: Vec<_> = read_line().trim_right().chars().map(|e| e == 'F').collect();
-
+        let f = |e| if e == 'M' { 1 } else { 0 };
+        let idol: Vec<_> = read_line().trim_right().chars().map(&f).collect();
+        let fans: Vec<_> = read_line().trim_right().chars().map(&f).collect();
         let result = solve(idol, fans);
         println!("{}", result);
     }
 }
 
-fn solve(idol: Vec<bool>, fans: Vec<bool>) -> u32 {
+fn solve(idol: Vec<u8>, fans: Vec<u8>) -> u32 {
     let end = fans.len() - idol.len() + 1;
 
     let mut count = 0;
 
-    for iter in 0..end {
+    for iter in (0..end).rev() {
         let subfan = &fans[iter..iter + idol.len()];
 
-        let mut result = true;
+        let mut is_all_zero = true;
         for i in 0..idol.len() {
-            if !(idol[i] || subfan[i]) {
-                result = false;
+            if idol[i] * subfan[i] == 1 {
+                is_all_zero = false;
                 break
             }
         }
 
-        if result {
+        if is_all_zero {
             count += 1
         }
 
-        // fn blank(count: usize) -> String {
-        //     " ".repeat(count)
-        // }
-        // fn print_people(input: &[bool]) -> String {
-        //     input.iter().map(|e| if *e { "F" } else { "M" }).collect()
-        // }
-        // let mut buffer = String::new();
-        // for i in 0..idol.len() { buffer += if idol[i] || subfan[i] { "_" } else { "X" }; }
-        // println!("{}{}", blank(iter), print_people(&idol));
-        // println!("{}{}", blank(iter), print_people(subfan));
-        // println!("{}{} {}", blank(iter), buffer, result);
-        // println!();
+        let blank = |count| " ".repeat(count);
+        let buffer: Vec<_> = (0..idol.len()).map(|i| idol[i] * subfan[i]).collect();
+        eprintln!("\n\n1: 냄저, 0: 여자");
+        eprintln!("{}", print_people(&fans));
+        eprintln!("{}{}", blank(iter), print_people(subfan));
+        eprintln!("{}{}", blank(iter), print_people(&idol));
+        eprintln!();
+        eprintln!("{}{} ({})", blank(iter), print_people(&buffer), if is_all_zero { "전부 0" } else { "1 있음" });
+        fn print_people(input: &[u8]) -> String { input.iter().map(|&e| if e == 1 { '1' } else { '0' }).collect() }
     }
 
     count

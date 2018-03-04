fbcc4f2b6efc297488f0395acfa7dd5f776fc83f
Hyeon Kim <simnalamburt@gmail.com>
2018-03-05 06:32:03 +0900

    PI: 재귀 없이 풀은 버전 만듦

--- PI/README.md
+++ PI/README.md
@@ -1,9 +1,13 @@
 [PI](https://algospot.com/judge/problem/read/PI)
 ========
 
+- recursion.rs : 클로저 재귀 + 메모이제이션. Interior mutability 사용함.
+- iter.rs : 재귀 없이 품.
+
 ```bash
 # 실행
-rustc recursion.rs -o a -O
+rustc -o a -O recursion.rs
+rustc -o a -O iter.rs
 
 time ./a < input
 time ./a < input2
@@ -11,7 +15,6 @@ time ./a < input2
 
 
 # 유닛테스트 실행
-rustc recursion.rs -o a --test
-
-./a
+rustc -o a --test recursion.rs && ./a
+rustc -o a --test iter.rs && ./a
 ```
---
+++ PI/iter.rs
@@ -0,0 +1,96 @@
+fn main() {
+    use std::io::{self, BufRead};
+
+    let stdin = io::stdin();
+    let mut handle = stdin.lock();
+    let mut read_line = || {
+        let mut buffer = String::new();
+        handle.read_line(&mut buffer).unwrap();
+        buffer
+    };
+
+    let count = read_line().trim_right().parse().unwrap();
+    for _ in 0..count {
+        let digits: Vec<_> = read_line().trim_right().chars().map(|e| e.to_digit(10).unwrap() as u8).collect();
+        let result = solve(&digits);
+
+        println!("{:?}", result);
+    }
+}
+
+fn solve(digits: &[u8]) -> u32 {
+    use std::cmp::min;
+
+    let mut cache = [0u32; 10001];
+    let len = digits.len();
+
+    for index in 3..len+1 {
+        cache[index] = match index {
+            0...2  => panic!("index는 항상 3 이상이어야 합니다."),
+            3 | 4 | 5 => eval(&digits[..index]),
+            _ => (3..min(index-2, 6))
+                    .map(|i| cache[index - i] + eval(&digits[index - i..index]))
+                    .min()
+                    .unwrap()
+        }
+    }
+
+    cache[len]
+}
+
+fn eval(digits: &[u8]) -> u32 {
+    let len = digits.len();
+
+    // 조각 길이 검사
+    if len < 3 || 5 < len {
+        panic!("조각의 길이는 항상 3 이상 5 이하여야 합니다. (입력된 조각의 길이: {})", len)
+    }
+
+    let diffs: Vec<_> = (1..len).map(|i| digits[i-1] as i8 - digits[i] as i8).collect();
+
+    // 단조증감여부 체크
+    let is_monotonic = diffs[1..].into_iter().all(|&e| diffs[0] == e);
+    if is_monotonic {
+        match diffs[0] {
+            0 => 1,
+            1 | -1 => 2,
+            _ => 5,
+        }
+    } else {
+        let is_alternative = (1..diffs.len()).all(|i| {
+            diffs[0] == (if i%2 == 1 { -1 } else { 1 } * diffs[i])
+        });
+
+        match is_alternative {
+            true => 4,
+            false => 10
+        }
+    }
+}
+
+#[test]
+fn test_eval() {
+    assert_eq!(1, eval(&[3, 3, 3]));
+    assert_eq!(1, eval(&[5, 5, 5, 5]));
+
+    assert_eq!(2, eval(&[2, 3, 4, 5, 6]));
+    assert_eq!(2, eval(&[3, 2, 1, 0]));
+
+    assert_eq!(4, eval(&[3, 2, 3]));
+    assert_eq!(4, eval(&[5, 4, 5, 4, 5]));
+
+    assert_eq!(5, eval(&[1, 4, 7]));
+    assert_eq!(5, eval(&[8, 6, 4, 2]));
+
+    assert_eq!(10, eval(&[1, 7, 9, 1, 2]));
+    assert_eq!(10, eval(&[3, 3, 1]));
+}
+
+#[test]
+fn test_solve() {
+    assert_eq!(4, solve(&[1, 2, 3, 4, 1, 2, 3, 4]));
+    assert_eq!(2, solve(&[1, 1, 1, 1, 1, 2, 2, 2]));
+    assert_eq!(5, solve(&[1, 2, 1, 2, 2, 2, 2, 2]));
+    assert_eq!(2, solve(&[2, 2, 2, 2, 2, 2, 2, 2]));
+    assert_eq!(14, solve(&[1, 2, 6, 7, 3, 9, 3, 9]));
+}

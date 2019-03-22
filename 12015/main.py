8144ee0e4892548d4c6eeb13062b9cc75023f45c
Hyeon Kim <simnalamburt@gmail.com>
2019-03-22 19:13:52 +0900

    12015: 알고리즘 개선하여 O(n^2)로 바꿈
    Thanks @kcm1700

--- 12015/main.py
+++ 12015/main.py
@@ -1,6 +1,7 @@
 import os
 import mmap
 from dataclasses import dataclass
+from bisect import bisect_left
 
 def read():
     '''
@@ -34,13 +35,37 @@ class Seq:
     len: int
     last: int
 
+    def __lt__(self, other_num: int):
+        '''
+        bisect_left 용 메서드
+        '''
+        return self.last < other_num
+
 def solution(numbers) -> int:
-    sequences = set()
+    # Invariant: sorted_seqs는 항상 seq.last 의 오름차순으로 정렬되어있음
+    sorted_seqs = []
+
+    # 나머지 숫자 입력
     for num in numbers:
-        sequences |= {
-            Seq(len=seq.len+1, last=num) for seq in sequences if seq.last < num
-        } | {Seq(len=1, last=num)}
-    return max(sequences, key=lambda e:e.len).len
+        # Invariant 검사
+        # TODO: Comment the line below
+        assert all(sorted_seqs[i].last <= sorted_seqs[i+1].last for i in range(len(sorted_seqs)-1))
+
+        idx = bisect_left(sorted_seqs, num)
+
+        # max 함수 정의상 len이 길이가 제일 큰것중 가장 앞에있는것(last가 가장
+        # 작은것)을 반환하게됨.
+        #
+        # Reference:
+        #   https://docs.python.org/3/library/functions.html#max
+        max_len = max((seq.len for seq in sorted_seqs[:idx]), default=0)
+
+        # 새 부분수열을 sorted_seqs의 적절한 위치에 삽입
+        sorted_seqs.insert(idx, Seq(len=max_len+1, last=num))
+
+        # TODO: 중복제거, 리스트에서 빼도 되는거 제거
+
+    return max(sorted_seqs, key=lambda e:e.len).len
 
 if __name__ == '__main__':
     print(solution(read()))

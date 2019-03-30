314e74ae049063595752ea46451c387e213919be
Hyeon Kim <simnalamburt@gmail.com>
2019-03-30 23:52:27 +0900

    2749: 기존 솔루션에서 메모이제이션 필요성 제거함
    @kcm1700 is god

--- 2749/solution.c
+++ 2749/solution.c
@@ -10,34 +10,42 @@ typedef int64_t i64;
 
 // Pisano period
 // Reference: https://en.wikipedia.org/wiki/Pisano_period
-static const i64 MOD = 1000000;
+static const i64 MODULO = 1000000;
 static const i64 PISANO_PERIOD = 1500000;
 
 static const u8 BASE_CASES[14] = { 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233 };
 
-static i64 fibo(const i64 num) {
+typedef struct { i64 _0, _1; } Pair;
+
+// fibo2(num) 은 num번째와 num+1 번째 피보나치 수를 반환하는 함수다
+static Pair fibo2(const i64 num) {
   // Base cases
-  if (num < 14) {
-    return BASE_CASES[num];
+  if (num < 13) {
+    return (Pair) { ._0 = BASE_CASES[num], ._1 = BASE_CASES[num+1] };
   }
 
   // d'Ocagne's identity
   // Reference: https://en.wikipedia.org/wiki/Fibonacci_number#d'Ocagne's_identity
   const i64 k = num / 2;
-  const i64 fibo_k0 = fibo(k);
-  const i64 fibo_k1 = fibo(k + 1);
-  const i64 answer = (
+  const Pair fibo_k = fibo2(k);
+  Pair answer = (
     num % 2 == 0 ?
-    fibo_k0*(2*fibo_k1 - fibo_k0) :
-    fibo_k1*fibo_k1 + fibo_k0*fibo_k0
-  ) % MOD;
-  return answer >= 0 ? answer : answer + MOD;
+    (Pair) { ._0 = fibo_k._0*(2*fibo_k._1 - fibo_k._0), ._1 = fibo_k._0*fibo_k._0 + fibo_k._1*fibo_k._1 } :
+    (Pair) { ._0 = fibo_k._0*fibo_k._0 + fibo_k._1*fibo_k._1, ._1 = fibo_k._1*(2*fibo_k._0 + fibo_k._1) }
+  );
+
+  // Mod out
+  answer._0 %= MODULO;
+  answer._1 %= MODULO;
+  if (answer._0 < 0) { answer._0 += MODULO; }
+  if (answer._1 < 0) { answer._1 += MODULO; }
+  return answer;
 }
 
 int main() {
   i64 num;
   scanf("%" SCNd64, &num);
 
-  const i64 answer = fibo(num % PISANO_PERIOD);
+  const i64 answer = fibo2(num % PISANO_PERIOD)._0;
   printf("%" PRId64 "\n", answer);
 }
--- 2749/solution.py
+++ 2749/solution.py
@@ -1,28 +1,34 @@
 # https://www.acmicpc.net/problem/2749
-from functools import lru_cache
 
 # Pisano period
 # Reference: https://en.wikipedia.org/wiki/Pisano_period
-MOD = 1000000
+MODULO = 1000000
 PISANO_PERIOD = 1500000
+BASE_CASES = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233]
 
-# lru_cache 붙이면 자동으로 memoization이 됨
-# Reference: https://docs.python.org/3/library/functools.html#functools.lru_cache
-@lru_cache(maxsize=None)
-def fibo(num: int) -> int:
+def fibo2(num: int) -> tuple:
+    '''
+    fibo2(num) 은 num번째와 num+1 번째 피보나치 수를 반환하는 함수다
+    '''
+
     # Base cases
-    if num < 14:
-        return [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233][num]
+    if num < 13:
+        return (BASE_CASES[num], BASE_CASES[num+1])
 
     # d'Ocagne's identity
     # Reference: https://en.wikipedia.org/wiki/Fibonacci_number#d'Ocagne's_identity
     k = num // 2
-    return (
-        fibo(k)*(2*fibo(k + 1) - fibo(k))
+    fibo_k0, fibo_k1 = fibo2(k)
+    answer0, answer1 = (
+        (fibo_k0*(2*fibo_k1 - fibo_k0), fibo_k0**2 + fibo_k1**2)
         if num % 2 == 0 else
-        fibo(k + 1)**2 + fibo(k)**2
-    ) % MOD
+        (fibo_k0**2 + fibo_k1**2, fibo_k1*(2*fibo_k0 + fibo_k1))
+    )
+
+    # Mod out
+    return (answer0 % MODULO, answer1 % MODULO)
 
 if __name__ == '__main__':
     num = int(input())
-    print(fibo(num % PISANO_PERIOD))
+    answer = fibo2(num % PISANO_PERIOD)[0]
+    print(answer)

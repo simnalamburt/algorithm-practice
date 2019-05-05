ef65fc395fd3e6b4fe0c17dfc3b7c6136d780348
Hyeon Kim <simnalamburt@gmail.com>
2019-05-06 03:13:02 +0900

    10826: Solved

---
+++ 10826/solution.py
@@ -0,0 +1,25 @@
+BASE_CASES = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233]
+
+def fibo2(num: int) -> tuple:
+    '''
+    fibo2(num) 은 num번째와 num+1 번째 피보나치 수를 반환하는 함수다
+    '''
+
+    # Base cases
+    if num < 13:
+        return (BASE_CASES[num], BASE_CASES[num+1])
+
+    # d'Ocagne's identity
+    # Reference: https://en.wikipedia.org/wiki/Fibonacci_number#d'Ocagne's_identity
+    k = num // 2
+    fibo_k0, fibo_k1 = fibo2(k)
+    return (
+        (fibo_k0*(2*fibo_k1 - fibo_k0), fibo_k0**2 + fibo_k1**2)
+        if num % 2 == 0 else
+        (fibo_k0**2 + fibo_k1**2, fibo_k1*(2*fibo_k0 + fibo_k1))
+    )
+
+if __name__ == '__main__':
+    num = int(input())
+    answer = fibo2(num)[0]
+    print(answer)

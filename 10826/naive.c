bac4f7989f3792216812fd430d2920c65aab0600
Hyeon Kim <simnalamburt@gmail.com>
2019-05-06 23:09:24 +0900

    10826: 큰수 처리를 가변길이 기반으로 바꿈, 1등

--- 10826/naive.c
+++ 10826/naive.c
@@ -1,9 +1,16 @@
-// gcc naive.c -O2 -Wall -Wextra -Wpedantic -Werror -lm -static -std=c11
+// on Linux:
+//   gcc naive.c -O2 -Wall -Wextra -Wpedantic -Werror -lm -static -std=c11
+//
+// on macOS:
+//   clang naive.c -O2 -Wall -Wextra -Wpedantic -Werror -lm -std=c11
+
+// Use only with gcc
+#if defined(__GNUC__) && !defined(__clang__)
 #pragma GCC optimize("O3")
 #pragma GCC target("arch=haswell")
+#endif
 
 #include <stdio.h>
-#include <stdlib.h>
 #include <stdint.h>
 #include <stdbool.h>
 #include <inttypes.h>
@@ -30,24 +37,60 @@ typedef uint64_t u64;
 // 이 프로그램이 출력하는 제일 큰 값은 피보나치 수열의 10000번째 항인데, 이
 // 숫자는 10**18 진법으로 표현할 경우 117개의 자리수로 표현할 수 있다. 그래서
 // BigInt를 길이 117의 배열로 표현하였다.
-#define BIGINT_LEN 117
+#define BIGINT_MAX_LEN 117
 
-typedef struct { u64 data[BIGINT_LEN]; } BigInt;
+typedef struct {
+  u8 len;
+  u64 data[BIGINT_MAX_LEN];
+} BigInt;
 
 // lhs와 rhs를 더한 결과를 self에 저장함
 static void bigint_add(BigInt *self, const BigInt *lhs, const BigInt *rhs) {
+  if (lhs->len < rhs->len) {
+    const BigInt *tmp;
+    // swap
+    tmp = lhs;
+    lhs = rhs;
+    rhs = tmp;
+  }
+
+  // assert(lhs->len >= rhs->len)
+  self->len = lhs->len;
+
   u64 carry = 0;
-  for (u8 i = 0; i < BIGINT_LEN; ++i) {
+  for (u8 i = 0; i < rhs->len; ++i) {
     const u64 num = lhs->data[i] + rhs->data[i] + carry;
     self->data[i] = num % BIGINT_RADIX;
     carry = num / BIGINT_RADIX;
   }
+  for (u8 i = rhs->len; i < lhs->len; ++i) {
+    const u64 num = lhs->data[i] + carry;
+    self->data[i] = num % BIGINT_RADIX;
+    carry = num / BIGINT_RADIX;
+  }
+
+  if (carry) {
+    self->data[self->len++] = carry;
+  }
+  // TODO: BIGINT_MAX_LEN 넘어가면 안됨
 }
 
 // param에 들어있는 값들을 복사하여 self에 저장함
 static void bigint_set(BigInt *self, const BigInt* param) {
-  for (u8 i = 0; i < BIGINT_LEN; ++i) {
+  self->len = param->len;
+  for (u8 i = 0; i < param->len; ++i) {
     self->data[i] = param->data[i];
+  }
+}
+
+static void bigint_set_u64(BigInt *self, u64 param) {
+  if (param) {
+    // non-zero
+    self->len = 1;
+    self->data[0] = param;
+  } else {
+    // zero
+    self->len = 0;
   }
 }
 
@@ -57,28 +100,7 @@ static void bigint_set(BigInt *self, const BigInt* param) {
 //   bigint_find_msd(0) => true
 //   bigint_find_msd(1234) => false
 static bool bigint_is_zero(const BigInt *self) {
-  for (u8 i = 0; i < BIGINT_LEN; ++i) {
-    if (self->data[i]) {
-      return false;
-    }
-  }
-  return true;
-}
-
-// self의 MSD가 몇번째 인덱스인지 출력함. self가 0일경우 -1 반환함
-//
-// Examples:
-//   bigint_find_msd(0) => -1
-//   bigint_find_msd(1234) => 0
-//   bigint_find_msd(10**20) => 1
-//   bigint_find_msd(10**40) => 2
-static i8 bigint_find_msd(const BigInt *self) {
-  for (i8 digit = BIGINT_LEN-1; digit >= 0; --digit) {
-    if (self->data[digit]) {
-      return digit;
-    }
-  }
-  return -1;
+  return self->len == 0;
 }
 
 // file에 self를 출력함
@@ -88,7 +110,7 @@ static void bigint_print(const BigInt *self, FILE* file) {
     return;
   }
 
-  const i8 msd_digit = bigint_find_msd(self);
+  const i8 msd_digit = ((i8)self->len) - 1;
 
   // 10**18진법에서 제일 큰 자리수부터 하나씩 출력함
   char buffer[BIGINT_RADIX_LOG10];
@@ -119,8 +141,9 @@ int main() {
   u16 num;
   scanf("%"SCNu16, &num);
 
-  BigInt a = { .data = {0} };
-  BigInt b = { .data = {1} };
+  BigInt a, b;
+  bigint_set_u64(&a, 0);
+  bigint_set_u64(&b, 1);
   while (num > 0) {
     BigInt tmp;
     bigint_add(&tmp, &a, &b);

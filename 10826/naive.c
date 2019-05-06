a2ab16560a4aa7f46d12d8bde8fea7a02c46e0f7
Hyeon Kim <simnalamburt@gmail.com>
2019-05-06 21:47:57 +0900

    10826: 10**37 진법에서 10**18 진법으로 구현체 바꿈

--- 10826/naive.c
+++ 10826/naive.c
@@ -11,34 +11,34 @@
 typedef uint8_t u8;
 typedef int8_t i8;
 typedef uint16_t u16;
-typedef __uint128_t u128;
+typedef uint64_t u64;
 
-// BigInt는 10**37 진법을 사용한다. 10**37 의 값이 BIGINT_RADIX에 저장되어있다.
+// BigInt는 10**18 진법을 사용한다. 10**18 의 값이 BIGINT_RADIX에 저장되어있다.
 //
-//     37 = floor(log(2**128, 10)) - 1
+//     18 = floor(log(2**64, 10)) - 1
 //
-// u128로 표현할 수 있는 제일 큰 10의 거듭제곱이 10**38 이기때문에 10**37 진법을
+// u64로 표현할 수 있는 제일 큰 10의 거듭제곱이 10**38 이기때문에 10**18 진법을
 // 쓰도록 결정했다. 덧셈할때에 carry 처리를 편하게 하기 위해 10**38 진법 대신
-// 10**37 진법을 쓰기로 하였다.
-#define BIGINT_RADIX ((u128)1000000000000 * (u128)1000000000000 * (u128)10000000000000)
-#define BIGINT_RADIX_LOG10 37
+// 10**18 진법을 쓰기로 하였다.
+#define BIGINT_RADIX 1000000000000000000
+#define BIGINT_RADIX_LOG10 18
 
 // BigInt의 자리수 한계
 //
-//     ceil(log(피보나치10000번째항, 10) / 37)
+//     ceil(log(피보나치10000번째항, 10) / 18)
 //
 // 이 프로그램이 출력하는 제일 큰 값은 피보나치 수열의 10000번째 항인데, 이
-// 숫자는 10**37 진법으로 표현할 경우 57개의 자리수로 표현할 수 있다. 그래서
-// BigInt를 길이 57의 배열로 표현하였다.
-#define BIGINT_LEN 57
+// 숫자는 10**18 진법으로 표현할 경우 117개의 자리수로 표현할 수 있다. 그래서
+// BigInt를 길이 117의 배열로 표현하였다.
+#define BIGINT_LEN 117
 
-typedef struct { u128 data[BIGINT_LEN]; } BigInt;
+typedef struct { u64 data[BIGINT_LEN]; } BigInt;
 
 // lhs와 rhs를 더한 결과를 self에 저장함
 static void bigint_add(BigInt *self, const BigInt *lhs, const BigInt *rhs) {
-  u128 carry = 0;
+  u64 carry = 0;
   for (u8 i = 0; i < BIGINT_LEN; ++i) {
-    const u128 num = lhs->data[i] + rhs->data[i] + carry;
+    const u64 num = lhs->data[i] + rhs->data[i] + carry;
     self->data[i] = num % BIGINT_RADIX;
     carry = num / BIGINT_RADIX;
   }
@@ -70,8 +70,8 @@ static bool bigint_is_zero(const BigInt *self) {
 // Examples:
 //   bigint_find_msd(0) => -1
 //   bigint_find_msd(1234) => 0
-//   bigint_find_msd(10**40) => 1
-//   bigint_find_msd(10**90) => 2
+//   bigint_find_msd(10**20) => 1
+//   bigint_find_msd(10**40) => 2
 static i8 bigint_find_msd(const BigInt *self) {
   for (i8 digit = BIGINT_LEN-1; digit >= 0; --digit) {
     if (self->data[digit]) {
@@ -90,12 +90,12 @@ static void bigint_fprint(const BigInt *self, FILE* file) {
 
   const i8 msd_digit = bigint_find_msd(self);
 
-  // 10**37진법에서 제일 큰 자리수부터 하나씩 출력함
+  // 10**18진법에서 제일 큰 자리수부터 하나씩 출력함
   char buffer[BIGINT_RADIX_LOG10];
   for (i8 digit = msd_digit; digit >= 0; --digit) {
-    u128 num = self->data[digit];
+    u64 num = self->data[digit];
 
-    // 10**37 진법의 자리수 하나 num 을 buffer에 10진수로 변환
+    // 10**18 진법의 자리수 하나 num 을 buffer에 10진수로 변환
     i8 j = BIGINT_RADIX_LOG10 - 1;
 
     for (; j >= 0; --j) {
@@ -108,7 +108,7 @@ static void bigint_fprint(const BigInt *self, FILE* file) {
       num /= 10;
     }
     ++j;
-    // 현재 buffer에 10진수 숫자가 '37 - j'개 만큼 기록되었음
+    // 현재 buffer에 10진수 숫자가 '18 - j'개 만큼 기록되었음
 
     // buffer 출력
     fwrite(&buffer[j], 1, BIGINT_RADIX_LOG10 - j, file);

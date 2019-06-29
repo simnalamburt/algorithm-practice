adfc29132ed9bdc0b89d49e4bf557743b47e6652
Hyeon Kim <simnalamburt@gmail.com>
2019-06-29 17:45:00 +0900

    12015: 입력에 문제있어서 WA 나옴. mmap 걷어내서 AC 받음

--- 12015/main.py
+++ 12015/main.py
@@ -1,30 +1,8 @@
-import os
-import mmap
 from bisect import bisect_left
 
 def read():
-    '''
-    stdin을 문제 스펙에 맞게 읽는 함수. stdin이 mmap(2)가 가능하지 않은 유형의
-    파일인 경우 mmap(2)으로 파일을 읽는다.
-    '''
-    try:
-        stdin = mmap.mmap(0, os.stat(0).st_size, mmap.MAP_PRIVATE, mmap.PROT_READ)
-    except OSError as e:
-        # mmap(2) 실패 이외의 이유로 실패했다면 예외 전파
-        if e.errno != 22:
-            raise
-
-        # mmap(2) 가 불가능한 유형의 파일이여서 실패했다면 .read()로 입력 읽기
-        count = int(input())
-        yield from (int(word) for word in input().split())
-        return
-
-    # mmap(2) 활용하여 빠르게 입력 처리
-    idx = stdin.find(b'\n')
-    count = int(stdin[:idx])
-    for _ in range(count):
-        pos, idx = idx, stdin.find(b' ', idx + 1)
-        yield int(stdin[pos:idx])
+    count = int(input())
+    yield from (int(word) for word in input().split())
 
 def solution(inputs) -> int:
     # seqs[i] 에는 길이가 i인 부분수열중 맨 뒤 항의 값이 들어있음

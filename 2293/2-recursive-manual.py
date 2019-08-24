5fd5f786ae2dfda5b61018a6beb70035cf417b43
Hyeon Kim <simnalamburt@gmail.com>
2019-08-24 20:31:02 +0900

    2293: 실수 수정

--- 2293/2-recursive-manual.py
+++ 2293/2-recursive-manual.py
@@ -3,8 +3,24 @@ from sys import stdin
 COINS_COUNT, TARGET = map(int, stdin.readline().split())
 COINS = sorted(map(int, stdin.read().split()), reverse=True)
 
-CACHE = [[-1]*(COINS_COUNT+1) for _ in range(TARGET+1)]
+def solve_actual(target: int, begin_index: int) -> int:
+    length = COINS_COUNT - begin_index
+    current_coin = COINS[begin_index]
 
+    # Handle base cases
+    if target == 0:
+        return 1
+    if length == 0:
+        return 0
+    if length == 1:
+        return 1 if target % current_coin == 0 else 0
+
+    return sum(
+        solve(target - i * current_coin, begin_index + 1)
+        for i in range(target//current_coin + 1)
+    )
+
+CACHE = [[-1]*(COINS_COUNT+1) for _ in range(TARGET+1)]
 def solve(target: int, begin_index: int) -> int:
     # target: [0, TARGET]
     # begin_index: [0, COINS_COUNT]
@@ -13,20 +29,7 @@ def solve(target: int, begin_index: int) -> int:
     if cache_entry != -1:
         return cache_entry
 
-    # Handle base cases
-    length = COINS_COUNT - begin_index
-    if target == 0:
-        return 1
-    if length == 0:
-        return 0
-    if length == 1:
-        return 1 if target % COINS[COINS_COUNT - 1] == 0 else 0
-
-    current_coin = COINS[begin_index]
-    result = sum(
-        solve(target - i * current_coin, begin_index + 1)
-        for i in range(target//current_coin + 1)
-    )
+    result = solve_actual(target, begin_index)
 
     CACHE[target][begin_index] = result
     return result

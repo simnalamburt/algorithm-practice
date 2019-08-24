1b992375592d2d586f2c7474a7a91f28b3f5c22f
Hyeon Kim <simnalamburt@gmail.com>
2019-08-24 22:24:12 +0900

    2294: 최적해 찾음

---
+++ 2294/main.py
@@ -0,0 +1,30 @@
+from sys import stdin
+from math import inf
+
+def solve(target: int, lines: list) -> int:
+    # counts[x]: 현재까지 입력받은 동전들을 사용해, 'x원'을 만들고자 할 때
+    # 필요한 최소 동전 수
+    counts = [0] + [inf]*target
+
+    # DP
+    for line in lines:
+        coin = int(line)
+        for current in range(coin, target+1):
+            counts[current] = min(
+                counts[current],
+                1 + counts[current - coin],
+            )
+
+    return counts[target]
+
+def main():
+    target = int(stdin.readline().split()[1])
+    lines = stdin.read().split()
+    answer = solve(target, lines)
+    if answer == inf:
+        print('-1')
+    else:
+        print(answer)
+
+if __name__ == '__main__':
+    main()

bd1bf2a9bcfcdcb380b1155748d45630806f7538
Hyeon Kim <simnalamburt@gmail.com>
2019-08-29 13:26:07 +0900

    7568: (misc) Update main.py

--- 7568/main.py
+++ 7568/main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # https://www.acmicpc.net/problem/7568
 from dataclasses import dataclass
+from sys import stdin
 
 # 데이터클래스
 # Reference: https://docs.python.org/3/library/dataclasses.html
@@ -14,8 +15,8 @@ class Person:
     def __lt__(self, other) -> bool:
         return self.weight < other.weight and self.height < other.height
 
-count = int(input())
-people = [Person(*map(int, input().split())) for _ in range(count)]
+stdin.readline()
+people = [Person(*map(int, line.split())) for line in stdin.readlines()]
 print(' '.join(
     str(sum(1 for e in people if person < e) + 1)
     for person in people

93a524250be1b0346cc4999a52500194543870fe
Hyeon Kim <simnalamburt@gmail.com>
2020-01-11 03:02:44 +0900

    10757: 10**18 진법 솔루션 만듦

--- 10757/solution.pas
+++ 10757/solution.pas
@@ -1,40 +1,93 @@
+// TODO: Remove me
+uses sysutils;
+
+type
+  bool = Boolean;
+  i16 = Word;
+  i32 = Int32;
+  i64 = Int64;
+  u64 = QWord;
+
+const
+  RADIX = 1000000000000000000;
+  RADIX_COUNT = 18;
+
+function find_begin(a: u64): u64;
+begin
+  if a >= RADIX_COUNT then
+    find_begin := a - RADIX_COUNT + 1
+  else
+    find_begin := 1
+end;
+
 procedure add_then_print(
   a, b: AnsiString;
-  len_a, len_b: Int64);
+  len_a, len_b: u64);
 var
-  buffer: Array Of Byte;
-  delta, i: Int64;
-  x, y, added, carry: Byte;
+  buffer: Array[0..556] Of u64; // 한칸당 RADIX_COUNT자리 10진수 하나
+  buffer_idx: i32 = 0;
+  buffer_size: i32;
+  carry: u64 = 0;
+  i: i64;
+  begin_a, begin_b, delta, x, y, added: u64;
+  _: i16;
+  pad_zero: bool = false;
 begin
   // len_a >= len_b
+  i := len_a;
   delta := len_a - len_b;
-  carry := 0;
-  setlength(buffer, len_a);
-  for i := len_a downto delta + 1 do begin
-    x := ord(a[i]) - 48;
-    y := ord(b[i - delta]) - 48;
+  while i > delta do begin
+    begin_a := find_begin(i);
+    begin_b := find_begin(i - delta);
+    val(copy(a, begin_a, i + 1 - begin_a), x, _);
+    val(copy(b, begin_b, i + 1 - delta - begin_b), y, _);
     added := x + y + carry;
 
-    carry := added div 10;
-    buffer[i-1] := added mod 10;
+    if added >= RADIX then begin
+      carry := 1;
+      added -= RADIX;
+    end else begin
+      carry := 0;
+    end;
+    buffer[buffer_idx] := added;
+    buffer_idx += 1;
+
+    i -= RADIX_COUNT;
   end;
-  for i := delta downto 1 do begin
-    x := ord(a[i]) - 48;
+  while i > 0 do begin
+    val(copy(a, i, find_begin(i)), x, _);
     added := x + carry;
 
-    carry := added div 10;
-    buffer[i-1] := added mod 10;
+    if added >= RADIX then begin
+      carry := 1;
+      added -= RADIX;
+    end else begin
+      carry := 0;
+    end;
+    buffer[buffer_idx] := added;
+    buffer_idx += 1;
+
+    i -= RADIX_COUNT;
   end;
-  if carry > 0 then write(carry);
-  for i := 0 to len_a - 1 do begin
-    write(buffer[i]);
+  if carry > 0 then begin
+    write(carry);
+    pad_zero := true;
+  end;
+  buffer_size := buffer_idx;
+  for buffer_idx := buffer_size - 1 downto 0 do begin
+    if pad_zero then begin
+      write(Format('%.18U', [buffer[buffer_idx]]));
+    end else begin
+      write(buffer[buffer_idx]);
+      pad_zero := true;
+    end;
   end;
   write(#10);
 end;
 
 var
   line: AnsiString;
-  len, len_a, len_b: Int64;
+  len, len_a, len_b: u64;
 begin
   read(line);
   len := length(line);

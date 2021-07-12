4220822ac740c891ca584df2fb6b817ba2acea7e
Hyeon Kim <simnalamburt@gmail.com>
2021-07-13 07:09:39 +0900

    1003: 1등

---
+++ 1003/solution.min.c
@@ -0,0 +1,1 @@
+c,I,J;char *B,*W;N(n,s){n=0;s=1;for(;;){c=B[I++];if(c<48)return s*n;n=10*n+c-48;}}P(n,b){n/10?P(n/10,0):0;n||b?W[J++]=n%10+48:0;}main;__libc_start_main(T,i,n,a,b,t){char m[1<<12],w[1<<12];B=m;read(0,B,1<<12);W=w;T=N();for(i=0;i<T;++i){for(n=N(),a=1,b=0;n>0;--n){t=a+b;a=b;b=t;}P(a,1);W[J++]=32;P(b,1);W[J++]=10;}write(1,W,J);_exit(0);}

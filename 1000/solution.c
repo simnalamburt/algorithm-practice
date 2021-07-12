df65e5bac29c4ae16b4973abf16108b39cadb927
Hyeon Kim <simnalamburt@gmail.com>
2021-07-13 04:47:42 +0900

    1000: 3등

--- 1000/solution.c
+++ 1000/solution.c
@@ -1,7 +1,1 @@
-#include <stdio.h>
-
-int main() {
-  int a, b;
-  scanf("%d%d", &a, &b);
-  printf("%d\n", a + b);
-}
+n;main;__libc_start_main(){read(0,&n,3);n=n%51-26;write(1,"1",n>9);n=n%10+48;write(1,&n,1);_exit(0);}

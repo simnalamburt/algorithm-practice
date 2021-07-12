78f26146f17c80a2e110c7042ea2d4ed2197f597
Hyeon Kim <simnalamburt@gmail.com>
2021-07-13 05:18:33 +0900

    1001: 2등

--- 1001/solution.c
+++ 1001/solution.c
@@ -1,1 +1,1 @@
-n;main;__libc_start_main(){read(0,&n,3);n=n%256-n/65536;if(n<0){write(1,"-",n<0);n=48-n;}else{n+=48;}write(1,&n,1);_exit(0);}
+n;main;__libc_start_main(){read(0,&n,3);n=n%64-(n>>16);n=(n<0?write(1,"-",1),-n:n)+48;write(1,&n,1);_exit(0);}

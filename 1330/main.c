10fb9ff83cd6782ba73ea51843e5f7eafb8d73ec
Hyeon Kim <simnalamburt@gmail.com>
2021-07-14 01:23:28 +0900

    1330: 2등

---
+++ 1330/main.c
@@ -0,0 +1,27 @@
+main;__libc_start_main() {
+  // read
+  char buf[99];
+  read(0, buf, 99);
+
+  // parse
+  int n[2] = {}, i = 0;
+  for (int idx = 0; idx < 2; ++idx) {
+    int sign = 1;
+    for (;;) {
+      char ch = buf[i++];
+      if (ch == '-') { sign = -1; }
+      else if (ch == ' ' || ch == '\n') { n[idx] *= sign; break; }
+      else { n[idx] = 10*n[idx] + ch - '0'; }
+    }
+  }
+
+  // write
+  if (n[0] > n[1]) {
+    write(1, ">", 1);
+  } else if (n[0] < n[1]) {
+    write(1, "<", 1);
+  } else {
+    write(1, "==", 2);
+  }
+  _exit(0);
+}

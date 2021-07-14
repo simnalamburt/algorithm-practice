d55924cc1d3ea1bf97d808b4ac6d2652ffc6a9b9
Hyeon Kim <simnalamburt@gmail.com>
2021-07-14 22:01:48 +0900

    2920: 1등

--- 2920/main.c
+++ 2920/main.c
@@ -1,18 +1,16 @@
 main;__libc_start_main() {
-  char buffer[15];
-  read(0, buffer, 15);
+  char buf[15];
+  read(0, buf, 15);
 
-  char asc[15] = "1 2 3 4 5 6 7 8";
-  char dsc[15] = "8 7 6 5 4 3 2 1";
-  int is_asc = 1, is_dsc = 1;
-
+  int asc = 1, dsc = 1;
   for (int i = 0; i < 15; ++i) {
-    is_asc &= buffer[i] == asc[i];
-    is_dsc &= buffer[i] == dsc[i];
+    asc &= buf[i] == "1 2 3 4 5 6 7 8"[i];
+    dsc &= buf[i] == "8 7 6 5 4 3 2 1"[i];
   }
-  if (is_asc) {
+
+  if (asc) {
     write(1, "ascending", 9);
-  } else if (is_dsc) {
+  } else if (dsc) {
     write(1, "descending", 10);
   } else {
     write(1, "mixed", 5);

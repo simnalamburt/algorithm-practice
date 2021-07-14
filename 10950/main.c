3da90c15059575c443aa6bcff31036ee9719405e
Hyeon Kim <simnalamburt@gmail.com>
2021-07-14 22:44:13 +0900

    10950: 1등

--- 10950/main.c
+++ 10950/main.c
@@ -1,35 +1,21 @@
-char *PTR;
+N,i,main;__libc_start_main() {
+  char buf[1024*8], *PTR = buf;
+  read(0, buf, 1024*8);
 
-int scan_uint() {
-  int n = 0;
   for (;;) {
     char c = *PTR++;
     if (c < '0') { break; }
-    n = 10*n + c - '0';
+    N = 10*N + c - '0';
   }
-  return n;
-}
 
-int main() {
-  char buffer[1024*32];
-  read(0, buffer, 1024*8);
-  PTR = buffer;
-
-  int N = scan_uint();
-  int i = 0;
-  for (int _ = 0; _ < N; ++_) {
-    int sum = PTR[0] - '0' + PTR[2] - '0';
-    if (sum >= 10) {
-      buffer[i++] = '1';
-      buffer[i++] = '0' + sum - 10;
-      buffer[i++] = '\n';
-    } else {
-      buffer[i++] = '0' + sum;
-      buffer[i++] = '\n';
-    }
+  while (N--) {
+    int sum = PTR[0] + PTR[2] - 96;
+    if (sum >= 10) { buf[i++] = '1'; }
+    buf[i++] = '0' + sum%10;
+    buf[i++] = '\n';
     PTR += 4;
   }
 
-  write(1, buffer, i);
+  write(1, buf, i);
   _exit(0);
 }

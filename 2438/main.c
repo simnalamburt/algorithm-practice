df8324a58520cb723be62c079aafadc29a66e258
Hyeon Kim <simnalamburt@gmail.com>
2021-07-14 17:43:37 +0900

    2438: 1등

--- 2438/main.c
+++ 2438/main.c
@@ -1,31 +1,22 @@
-#include <unistd.h>
-
-char *PTR;
-int read_uint() {
-  for (int n = 0;;) {
-    char ch = *PTR++;
-    if ('0' <= ch && ch <= '9') {
-      n = 10*n + ch - '0';
-    } else {
-      return n;
-    }
+int parse_uint(char buf[], int size) {
+  int n = 0;
+  for (int i = 0; i < size; ++i) {
+    char c = buf[i];
+    if (c < '0' || '9' < c) { break; }
+    n = 10*n + c - '0';
   }
+  return n;
 }
 
 int __libc_start_main() {
   char buf[5150];
-  read(0, buf, 9);
-  PTR = buf;
-
-  // read
-  int N = read_uint();
+  int size = read(0, buf, 9);
+  int N = parse_uint(buf, size);
 
   // buffer
   int i = 0;
   for (int a = 1; a <= N; ++a) {
-    for (int b = 1; b <= a; ++b) {
-      buf[i++] = '*';
-    }
+    for (int b = 0; b < a; ++b) { buf[i++] = '*'; }
     buf[i++] = '\n';
   }
 

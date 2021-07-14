fc8c7cda373cfede72ba44c62e9122a9595b85ec
Hyeon Kim <simnalamburt@gmail.com>
2021-07-15 00:48:55 +0900

    3052: 1등

--- 3052/main.c
+++ 3052/main.c
@@ -1,12 +1,20 @@
-#include <stdio.h>
-#include <stdint.h>
+main;__libc_start_main() {
+  char buf[50];
+  read(0, buf, 50);
 
-int main() {
-  uint64_t bitset = 0;
+  unsigned long long bitset = 0;
+  int a = 0, c;
   for (int i = 0; i < 10; ++i) {
-    int n;
-    scanf("%d", &n);
-    bitset |= 1lu << (n%42);
+    int n = 0;
+    while ((c = buf[a++]) >= '0') { n = 10*n + c - '0'; }
+    bitset |= 1llu << (n%42);
   }
-  printf("%d\n", __builtin_popcountll(bitset));
+  int count = __builtin_popcountll(bitset);
+  if (count == 10) {
+    write(1, "10", 2);
+  } else {
+    buf[0] = count + '0';
+    write(1, buf, 1);
+  }
+  _exit(0);
 }

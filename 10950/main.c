b4d630647cf07c78e744d6d639d3e2008b6a3090
Hyeon Kim <simnalamburt@gmail.com>
2021-07-14 22:51:45 +0900

    10950: 1등, 코드 줄임

--- 10950/main.c
+++ 10950/main.c
@@ -1,19 +1,16 @@
-N,i,main;__libc_start_main() {
-  char buf[1024*8], *PTR = buf;
-  read(0, buf, 1024*8);
+a,i,main;__libc_start_main(size) {
+  char buf[1024*8];
+  size = read(0, buf, 1024*8);
 
-  for (;;) {
-    char c = *PTR++;
-    if (c < '0') { break; }
-    N = 10*N + c - '0';
-  }
+  while (buf[a++] >= '0');
 
-  while (N--) {
-    int sum = PTR[0] + PTR[2] - 96;
+  while (a < size) {
+    int sum = buf[a] + buf[a+2] - 96;
+    a += 4;
+
     if (sum >= 10) { buf[i++] = '1'; }
     buf[i++] = '0' + sum%10;
     buf[i++] = '\n';
-    PTR += 4;
   }
 
   write(1, buf, i);

da0bcb03d0bec55083871724cfbb767f0b200103
Hyeon Kim <simnalamburt@gmail.com>
2021-07-14 22:53:33 +0900

    10952: 1등

---
+++ 10952/main.c
@@ -0,0 +1,16 @@
+a,i,main;__libc_start_main(size) {
+  char buf[1024*8];
+  size = read(0, buf, 1024*8)-4;
+
+  while (a < size) {
+    int sum = buf[a] + buf[a+2] - 96;
+    a += 4;
+
+    if (sum >= 10) { buf[i++] = '1'; }
+    buf[i++] = '0' + sum%10;
+    buf[i++] = '\n';
+  }
+
+  write(1, buf, i);
+  _exit(0);
+}

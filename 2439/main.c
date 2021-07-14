080a68a0ea1ba87987a8f5beec01ab260c7c78ec
Hyeon Kim <simnalamburt@gmail.com>
2021-07-14 17:29:09 +0900

    2439: libc 배제

--- 2439/main.c
+++ 2439/main.c
@@ -1,18 +1,23 @@
-#include <stdio.h>
-#include <unistd.h>
+int parse_uint(char buf[], int size) {
+  int n = 0;
+  for (int i = 0; i < size; ++i) {
+    char c = buf[i];
+    if (c < '0' || '9' < c) { break; }
+    n = 10*n + c - '0';
+  }
+  return n;
+}
 
 int main() {
-  // read
-  int N;
-  scanf("%d", &N);
+  char buf[10100];
+  int size = read(0, buf, 3);
+  int N = parse_uint(buf, size);
 
   // buffer
   int i = 0;
-  char buf[10100];
   for (int a = 1; a <= N; ++a) {
-    int b = N;
-    for (; b > a; --b) { buf[i++] = ' '; }
-    for (; b > 0; --b) { buf[i++] = '*'; }
+    for (int b = 0; b < N - a; ++b) { buf[i++] = ' '; }
+    for (int b = 0; b < a; ++b) { buf[i++] = '*'; }
     buf[i++] = '\n';
   }
 

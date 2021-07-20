a85c0e6307e4f34a922b74a78a56f96650d2ca40
Hyeon Kim <simnalamburt@gmail.com>
2021-07-21 08:05:43 +0900

    10816: 2등

--- 10816/main.c
+++ 10816/main.c
@@ -2,39 +2,25 @@
 #pragma GCC target("arch=haswell")
 #include <unistd.h>
 
-#define BUF_SIZE (1024*4)
+#define BUF_SIZE (1024*1024*10)
+#define WBUF_SIZE (1024*1024*10)
 
-char *BUF, *WBUF;
-int C = BUF_SIZE;
-int scan_ch() {
-  if (C == BUF_SIZE) { read(0, BUF, BUF_SIZE); C = 0; }
-  return BUF[C++];
-}
-
+char *BUF;
+int C = 0;
 int scan_int() {
   int n = 0, sign = 1, c;
-  while ((c = scan_ch()) >= '-') {
+  while ((c = BUF[C++]) >= '-') {
     if (c == '-') { sign = -1; }
     else { n = 10*n + c - '0'; }
   }
   return n * sign;
 }
 
+char *WBUF;
 int W = 0;
-void flush() {
-  write(1, WBUF, W);
-  W = 0;
-}
-
-void print_ch(int ch) {
-  if (W == BUF_SIZE) { flush(); }
-  WBUF[W++] = ch;
-}
-
-void print_int(int n) {
-  if (n < 0) { print_ch('-'); print_int(-n); return; }
-  if (n/10) { print_int(n/10); }
-  print_ch('0' + n%10);
+void print_uint(int n) {
+  if (n/10) { print_uint(n/10); }
+  WBUF[W++] = '0' + n%10;
 }
 
 int main() { }
@@ -42,7 +28,8 @@ int main() { }
 int count[20000001];
 
 int __libc_start_main() {
-  char buf[BUF_SIZE], wbuf[BUF_SIZE];
+  char buf[BUF_SIZE], wbuf[WBUF_SIZE];
+  read(0, buf, BUF_SIZE);
   BUF = buf, WBUF = wbuf;
 
   int N = scan_int();
@@ -50,13 +37,13 @@ int __libc_start_main() {
     ++count[10000000 + scan_int()];
   }
   int M = scan_int();
-  print_int(count[10000000 + scan_int()]);
+  print_uint(count[10000000 + scan_int()]);
   for (int i = 1; i < M; ++i) {
-    print_ch(' ');
-    print_int(count[10000000 + scan_int()]);
+    WBUF[W++] = ' ';
+    print_uint(count[10000000 + scan_int()]);
   }
-  print_ch('\n');
+  WBUF[W++] = '\n';
 
-  flush();
+  write(1, WBUF, W);
   _exit(0);
 }

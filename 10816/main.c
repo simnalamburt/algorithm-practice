b55ecc1967b9dd26f7bdd68160c2b4a081f2dfad
Hyeon Kim <simnalamburt@gmail.com>
2021-07-21 08:23:07 +0900

    10816: mmap 사용

--- 10816/main.c
+++ 10816/main.c
@@ -1,13 +1,12 @@
 #pragma GCC optimize("O3")
 #pragma GCC target("arch=haswell")
+#include <sys/stat.h>
+#include <sys/mman.h>
 #include <unistd.h>
 
-#define BUF_SIZE (1024*1024*10)
-#define WBUF_SIZE (1024*1024*10)
-
 char *BUF;
-int C = 0;
 int scan_int() {
+  static int C = 0;
   int n = 0, sign = 1, c;
   while ((c = BUF[C++]) >= '-') {
     if (c == '-') { sign = -1; }
@@ -16,7 +15,7 @@ int scan_int() {
   return n * sign;
 }
 
-char *WBUF;
+char WBUF[1024*1024*4];
 int W = 0;
 void print_uint(int n) {
   if (n/10) { print_uint(n/10); }
@@ -28,9 +27,16 @@ int main() { }
 int count[20000001];
 
 int __libc_start_main() {
-  char buf[BUF_SIZE], wbuf[WBUF_SIZE];
-  read(0, buf, BUF_SIZE);
-  BUF = buf, WBUF = wbuf;
+  // stdin 길이 측정
+  struct stat stat;
+  const int ret = fstat(0, &stat);
+  if (ret != 0) { _exit(-1); }
+  const off_t file_size = stat.st_size;
+
+  // stdin mmap 수행
+  BUF = mmap(NULL, file_size, PROT_READ, MAP_PRIVATE, 0, 0);
+  if (BUF == MAP_FAILED) { _exit(-1); }
+
 
   int N = scan_int();
   for (int i = 0; i < N; ++i) {

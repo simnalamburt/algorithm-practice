f3a6eacaf6226a741c3adb61cd2947972af0af15
Hyeon Kim <simnalamburt@gmail.com>
2021-07-21 04:41:55 +0900

    10828: 1등

--- 10828/main.c
+++ 10828/main.c
@@ -1,73 +1,38 @@
-#include <unistd.h>
-
 char *PTR;
-
-int scan_uint() {
+scan() {
   int n = 0, c;
   while ((c = *PTR++) >= '0') { n = 10*n + c - '0'; }
   return n;
 }
 
-int print_uint(int n, char buf[]) {
-  int offset = 0;
-  if (n/10) { offset += print_uint(n/10, buf); }
-  buf[offset] = '0' + n%10;
-  return offset + 1;
+char *WPTR;
+print(n) {
+  if (n < 0) { *WPTR++ = '-'; print(-n); return; }
+  if (n/10) { print(n/10); }
+  *WPTR++ = '0' + n%10;
 }
 
-_Bool is_equal(char left[], char right[]) {
-  for (int i = 0; left[i]; ++i) {
-    if (left[i] != right[i]) { return 0; }
-  }
+cmd(char *l) {
+  char *r = PTR;
+  for (; *l; ++l, ++r) { if (*l != *r) { return 0; } }
   return 1;
 }
 
-int main() { }
+main;__libc_start_main() {
+  char buf[1024 * 64], wbuf[1024 * 32];
+  read(0, buf, 1024 * 64);
+  PTR = buf, WPTR = wbuf;
 
-int __libc_start_main() {
-  char buf[120006], wbuf[70000];
-  read(0, buf, 120006);
-  PTR = buf;
-
-  int N = scan_uint(), i = 0, stack[N], top = 0;
+  int N = scan(), stack[N], top = 0;
   for (int _ = 0; _ < N; ++_) {
-    if (is_equal("push", PTR)) {
-      PTR += 5;
-
-      stack[top++] = scan_uint();
-    } else if (is_equal("pop", PTR)) {
-      PTR += 4;
-
-      if (!top) {
-        wbuf[i++] = '-';
-        wbuf[i++] = '1';
-      } else {
-        i += print_uint(stack[--top], &wbuf[i]);
-      }
-      wbuf[i++] = '\n';
-    } else if (is_equal("size", PTR)) {
-      PTR += 5;
-
-      i += print_uint(top, &wbuf[i]);
-      wbuf[i++] = '\n';
-    } else if (is_equal("empty", PTR)) {
-      PTR += 6;
-
-      i += print_uint(!top, &wbuf[i]);
-      wbuf[i++] = '\n';
-    } else if (is_equal("top", PTR)) {
-      PTR += 4;
-
-      if (!top) {
-        wbuf[i++] = '-';
-        wbuf[i++] = '1';
-      } else {
-        i += print_uint(stack[top-1], &wbuf[i]);
-      }
-      wbuf[i++] = '\n';
-    }
+    if (cmd("push")) { PTR += 5; stack[top++] = scan(); continue; }
+    else if (cmd("pop")) { PTR += 4; print(top ? stack[--top] : -1); }
+    else if (cmd("size")) { PTR += 5; print(top); }
+    else if (cmd("empty")) { PTR += 6; print(!top); }
+    else if (cmd("top")) { PTR += 4; print(top ? stack[top-1] : -1); }
+    *WPTR++ = '\n';
   }
 
-  write(1, wbuf, i);
+  write(1, wbuf, WPTR - wbuf);
   _exit(0);
 }

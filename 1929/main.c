764e1a56dd830b596a6e56d4df9f3b81503484a1
Hyeon Kim <simnalamburt@gmail.com>
2021-07-28 05:39:54 +0900

    1929: inverted_sieve 사용

--- 1929/main.c
+++ 1929/main.c
@@ -19,23 +19,23 @@ inline static _Bool nth(__uint8_t bitset[], int n) {
   return bitset[n/8] & (1 << (n%8));
 }
 
-inline static void unset(__uint8_t bitset[], int n) {
-  bitset[n/8] &= ~(1 << (n%8));
+inline static void set(__uint8_t bitset[], int n) {
+  bitset[n/8] |= 1 << (n%8);
 }
 
 int main() {
   read(0, BUF, sizeof BUF);
 
   int M = scan_uint(), L = scan_uint() + 1;
-  __uint8_t sieve[L/8 + 1];
-  sieve[0] = 0b11111100;
-  for (int i = 1; i < L/8 + 1; ++i) { sieve[i] = 0b11111111; }
+  __uint8_t inverted_sieve[L/8 + 1];
+  inverted_sieve[0] = 3;
+  for (int i = 1; i < L/8 + 1; ++i) { inverted_sieve[i] = 0; }
   for (int i = 2; i*i < L; ++i) {
-    if (!nth(sieve, i)) { continue; }
-    for (int j = i*i; j < L; j += i) { unset(sieve, j); }
+    if (nth(inverted_sieve, i)) { continue; }
+    for (int j = i*i; j < L; j += i) { set(inverted_sieve, j); }
   }
   for (int i = M; i < L; ++i) {
-    if (!nth(sieve, i)) { continue; }
+    if (nth(inverted_sieve, i)) { continue; }
     print_uint(i);
     WBUF[W++] = '\n';
   }

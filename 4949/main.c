416ac1d3d5349818de05ca6aeaee7f76631750df
Hyeon Kim <simnalamburt@gmail.com>
2021-07-29 13:20:17 +0900

    4949: 0ms

--- 4949/main.c
+++ 4949/main.c
@@ -1,31 +1,53 @@
-#include <stdio.h>
-#include <string.h>
+#include <unistd.h>
 
-_Bool solve(const char *line) {
+char BUF[1024 * 1024 * 16], WBUF[1024 * 1024 * 16];
+int C = 0, W = 0;
+
+_Bool solve() {
   char stack[100];
-  int top = 0, ch;
-  for (int i = 0; (ch = line[i]) != 0; ++i) {
+  int top = 0;
+  for (;;) {
+    char ch = BUF[C++];
     switch (ch) {
       case '(':
       case '[':
         stack[top++] = ch;
         break;
       case ')':
-        if (top > 0 && stack[top-1] == '(') { --top; }
-        else { return 0; }
+        if (top <= 0 || stack[top-1] != '(') { goto FAILED; }
+        --top;
         break;
       case ']':
-        if (top > 0 && stack[top-1] == '[') { --top; }
-        else { return 0; }
+        if (top <= 0 || stack[top-1] != '[') { goto FAILED; }
+        --top;
         break;
+      case '\n':
+        return top == 0;
     }
   }
-  return top == 0;
+FAILED:
+  while (BUF[C++] != '\n');
+  return 0;
 }
 
-int main() {
-  char line[102];
-  while (fgets(line, 102, stdin), strcmp(line, ".\n") != 0) {
-    puts(solve(line) ? "yes" : "no");
+int main() { }
+int __libc_start_main() {
+  read(0, BUF, sizeof BUF);
+
+  for (;;) {
+    if (BUF[C] == '.' && BUF[C+1] == '\n') { break; }
+    if (solve()) {
+      WBUF[W++] = 'y';
+      WBUF[W++] = 'e';
+      WBUF[W++] = 's';
+      WBUF[W++] = '\n';
+    } else {
+      WBUF[W++] = 'n';
+      WBUF[W++] = 'o';
+      WBUF[W++] = '\n';
+    }
   }
+
+  write(1, WBUF, W);
+  _exit(0);
 }

a48a9e3dc60dbc18d26439a5e9ef356f728b003e
Hyeon Kim <simnalamburt@gmail.com>
2021-07-29 13:56:31 +0900

    4949: sizeof 배제

--- 4949/main.c
+++ 4949/main.c
@@ -1,10 +1,12 @@
 #include <unistd.h>
+#define BUF_SIZE (1024*8)
+#define WBUF_SIZE (1024*2)
 
-char BUF[1024 * 8], WBUF[1024 * 2];
-int C = sizeof BUF, W = 0;
+char BUF[BUF_SIZE], WBUF[WBUF_SIZE];
+int C = BUF_SIZE, W = 0;
 
 char scan_ch() {
-  if (C >= sizeof BUF) { read(0, BUF, sizeof BUF); C = 0; }
+  if (C >= BUF_SIZE) { read(0, BUF, BUF_SIZE); C = 0; }
   return BUF[C++];
 }
 
@@ -40,7 +42,7 @@ int __libc_start_main() {
   for (;;) {
     if (BUF[C] == '.' && BUF[C+1] == '\n') { break; }
 
-    if (W + 4 >= sizeof WBUF) { write(1, WBUF, W); W = 0; }
+    if (W + 4 >= WBUF_SIZE) { write(1, WBUF, W); W = 0; }
     if (solve()) {
       WBUF[W++] = 'y';
       WBUF[W++] = 'e';

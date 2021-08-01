765bc8e13af6e1d63ff6ad7b3f49c291f5e41df4
Hyeon Kim <simnalamburt@gmail.com>
2021-08-01 15:25:12 +0900

    1874: println_ch() 함수 분리

--- 1874/main.c
+++ 1874/main.c
@@ -1,11 +1,18 @@
 #include <unistd.h>
 
-char BUF[588902], WBUF[400000];
+char BUF[588902];
 int scan_uint() {
   static int C = 0;
   int n = 0, c;
   while ((c = BUF[C++]) >= '-') { n = 10*n + c - '0'; }
   return n;
+}
+
+char WBUF[400000];
+int W = 0;
+void println_ch(char ch) {
+  WBUF[W++] = ch;
+  WBUF[W++] = '\n';
 }
 
 int stack[100000];
@@ -14,18 +21,16 @@ int main() { }
 int __libc_start_main() {
   read(0, BUF, sizeof BUF);
 
-  int n = scan_uint(), stack_len = 0, upcoming = 1, W = 0;
+  int n = scan_uint(), stack_len = 0, upcoming = 1;
   for (int _ = 0; _ < n; ++_) {
     int X = scan_uint();
     while (X >= upcoming) {
       stack[stack_len++] = upcoming++;
-      WBUF[W++] = '+';
-      WBUF[W++] = '\n';
+      println_ch('+');
     }
     if (stack_len > 0 && stack[stack_len - 1] == X) {
       --stack_len;
-      WBUF[W++] = '-';
-      WBUF[W++] = '\n';
+      println_ch('-');
     } else {
       goto IMPOSSIBLE;
     }

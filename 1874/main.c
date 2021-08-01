cfa98ff7eb1e200702aeb5c67ab82ce766142248
Hyeon Kim <simnalamburt@gmail.com>
2021-08-01 15:14:06 +0900

    1874: cmds 없앰

--- 1874/main.c
+++ 1874/main.c
@@ -9,33 +9,29 @@ int scan_uint() {
 }
 
 int main() {
-  char buf[588902], WBUF[400000];
+  char buf[588902];
   BUF = buf;
   read(0, buf, sizeof buf);
 
-  int n = scan_uint();
-  int stack[n], stack_len = 0;
-  char cmds[n*2]; int cmds_len = 0;
-  int upcoming = 1;
+  int n = scan_uint(), stack[n], stack_len = 0, upcoming = 1;
+  char WBUF[n*4]; int W = 0;
+
   for (int _ = 0; _ < n; ++_) {
     int X = scan_uint();
     while (X >= upcoming) {
       stack[stack_len++] = upcoming++;
-      cmds[cmds_len++] = '+';
+      WBUF[W++] = '+';
+      WBUF[W++] = '\n';
     }
     if (stack_len > 0 && stack[stack_len - 1] == X) {
       --stack_len;
-      cmds[cmds_len++] = '-';
+      WBUF[W++] = '-';
+      WBUF[W++] = '\n';
     } else {
       goto IMPOSSIBLE;
     }
   }
 
-  int W = 0;
-  for (int i = 0; i < cmds_len; ++i) {
-    WBUF[W++] = cmds[i];
-    WBUF[W++] = '\n';
-  }
   write(1, WBUF, W);
   _exit(0);
 

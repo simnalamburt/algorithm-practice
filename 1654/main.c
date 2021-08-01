212590bff0abffe0364e61fa5fb7132444e066b9
Hyeon Kim <simnalamburt@gmail.com>
2021-08-01 13:09:28 +0900

    1654: 1등

---
+++ 1654/main.c
@@ -0,0 +1,41 @@
+#include <unistd.h>
+
+char *BUF;
+int scan_uint() {
+  static int C = 0;
+  int n = 0, c;
+  while ((c = BUF[C++]) >= '-') { n = 10*n + c - '0'; }
+  return n;
+}
+
+char *WBUF;
+int W = 0;
+void print_uint(int n) {
+  if (n/10) { print_uint(n/10); }
+  WBUF[W++] = '0' + n%10;
+}
+
+int main() { }
+int __libc_start_main() {
+  char buf[110014];
+  BUF = WBUF = buf;
+  read(0, buf, sizeof buf);
+
+  int K = scan_uint(), N = scan_uint();
+  int cables[K];
+  for (int i = 0; i < K; ++i) {
+    cables[i] = scan_uint();
+  }
+
+  long lo = 1, hi = 0x80000000L;
+  while (lo + 1 < hi) {
+    long mid = (lo + hi)/2;
+    long count = 0;
+    for (int i = 0; i < K; ++i) { count += (long)cables[i] / mid; }
+    *(count >= N ? &lo : &hi) = mid;
+  }
+
+  print_uint(lo);
+  write(1, WBUF, W);
+  _exit(0);
+}

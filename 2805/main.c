887f2f5adfd142ba1d38d6c0145c5abacf04b7a1
Hyeon Kim <simnalamburt@gmail.com>
2021-08-01 19:11:17 +0900

    2805: 32ms, 1등

--- 2805/main.c
+++ 2805/main.c
@@ -1,16 +1,18 @@
+#pragma GCC optimize("O4,unroll-loops")
+#pragma GCC target("arch=haswell")
 #include <unistd.h>
 
-char BUF[1024 * 1024 * 20];
-int scan_uint() {
+static char BUF[1024 * 1024 * 20];
+static int scan_uint() {
   static int C = 0;
   int n = 0, c;
   while ((c = BUF[C++]) >= '-') { n = 10*n + c - '0'; }
   return n;
 }
 
-char *WBUF = BUF;
-int W = 0;
-void print_uint(int n) {
+static char WBUF[64];
+static int W = 0;
+static void print_uint(int n) {
   if (n/10) { print_uint(n/10); }
   WBUF[W++] = '0' + n%10;
 }

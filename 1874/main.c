18c51113fd9a65acff0bbfbc43e95aa2050df86f
Hyeon Kim <simnalamburt@gmail.com>
2021-08-01 16:05:34 +0900

    1874: 1등, 읽기 스트리밍 구현

--- 1874/main.c
+++ 1874/main.c
@@ -1,8 +1,17 @@
 #include <unistd.h>
+#define BUF_SIZE (1024 * 32)
 
-char BUF[588902];
+char BUF[BUF_SIZE];
 int scan_uint() {
-  static int C = 0;
+  static int C = BUF_SIZE;
+
+  int remain = BUF_SIZE - C;
+  if (remain <= 15) {
+    for (int i = 0; i < remain; ++i) { BUF[i] = BUF[C + i]; }
+    read(0, BUF + remain, BUF_SIZE - remain);
+    C = 0;
+  }
+
   int n = 0, c;
   while ((c = BUF[C++]) >= '-') { n = 10*n + c - '0'; }
   return n;
@@ -19,8 +28,6 @@ int stack[100000];
 
 int main() { }
 int __libc_start_main() {
-  read(0, BUF, sizeof BUF);
-
   int n = scan_uint(), stack_len = 0, upcoming = 1;
   for (int _ = 0; _ < n; ++_) {
     int X = scan_uint();

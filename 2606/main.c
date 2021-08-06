19d6cd2cbb2357fa25c720443222cf919d79e1aa
Hyeon Kim <simnalamburt@gmail.com>
2021-08-06 19:15:24 +0900

    2606: 1등

--- 2606/main.c
+++ 2606/main.c
@@ -8,44 +8,39 @@ int scan_uint() {
   return n;
 }
 
-char *WBUF;
-int W = 0;
-void print_uint(int n) {
-  if (n/10) { print_uint(n/10); }
-  WBUF[W++] = '0' + n%10;
-}
-
 typedef __uint128_t u128;
 u128 graph[128], I = 1;
 
 int main() { }
 int __libc_start_main() {
   char buf[39609];
-  BUF = WBUF = buf;
+  BUF = buf;
   read(0, buf, sizeof buf);
 
-  int V = scan_uint(), E = scan_uint();
+  scan_uint();
+  int E = scan_uint();
   for (int _ = 0; _ < E; ++_) {
     int a = scan_uint(), b = scan_uint();
-    graph[a] |= (I << b);
-    graph[b] |= (I << a);
+    graph[a] |= I << b;
+    graph[b] |= I << a;
   }
 
-  u128 visited = 0;
-
-  u128 tasks = 0x2;
+  u128 visited = 0, tasks = 0x2;
   while (tasks) {
     int task = 0;
-    for (u128 t = tasks; !(t & 1); ++task, t >>= 1) { }
-    tasks &= ~(I << task);
-    visited |= (I << task);
+    u128 T = 1;
+    while (!(tasks & T)) { ++task; T <<= 1; }
+    tasks &= ~T;
+    visited |= T;
     tasks |= graph[task] & ~visited;
   }
 
   int count = -1;
   for (; visited; visited >>= 1) { count += visited & 1; }
 
-  print_uint(count);
-  write(1, WBUF, W);
+  int W = 0;
+  if (count / 10) { BUF[W++] = '0' + count/10; }
+  BUF[W++] = '0' + count%10;
+  write(1, BUF, W);
   _exit(0);
 }

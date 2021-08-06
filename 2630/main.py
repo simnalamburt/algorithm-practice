ab8596e657dcc2ca96eb3363e12ecbc6404bebde
Hyeon Kim <simnalamburt@gmail.com>
2021-08-06 19:52:03 +0900

    2630: AC

--- 2630/main.py
+++ 2630/main.py
@@ -1,22 +1,28 @@
 N = int(input())
-MAP = [[*map(int, input().split())] for _ in range(N)]
+MAP = [[*map(int, input().split())] for _ in range(N)] # 0: white, 1: blue
 
-DP = [[1]*N for _ in range(N)]
+def solve(i: int, j: int, n: int) -> tuple[int, int]:
+    # Invariant:
+    #   n is power of 2
+    #   i % n == 0
+    #   j % n == 0
+    #
+    # Returns: (# of white, # of blue)
+    if n == 1:
+        return (1, 0) if MAP[i][j] == 0 else (0, 1)
 
-k = 1
-while k < N:
-    for i in range(0, N, k*2):
-        for j in range(0, N, k*2):
-            if (
-                DP[i][j] == DP[i+k][j] == DP[i][j+k] == DP[i+k][j+k] == 1 and
-                MAP[i][j] == MAP[i+k][j] == MAP[i][j+k] == MAP[i+k][j+k]
-            ):
-                # Nothing to do
-                ...
-            else:
-                DP[i][j] = DP[i][j] + DP[i+k][j] + DP[i][j+k] + DP[i+k][j+k]
-    k *= 2
+    k = n//2
+    a = solve(i,     j,     k)
+    b = solve(i + k, j,     k)
+    c = solve(i,     j + k, k)
+    d = solve(i + k, j + k, k)
+    if a == b == c == d and (a == (1, 0) or a == (0, 1)):
+        return a
 
-print(DP[0][0])
+    return (a[0] + b[0] + c[0] + d[0], a[1] + b[1] + c[1] + d[1])
 
-# NOTE: 입출력 스펙 똑바로 안읽었음
+answer = solve(0, 0, N)
+print(answer[0])
+print(answer[1])
+
+# NOTE: 입출력 스펙 똑바로 안읽었음, join 하기 전에 str로 안만들었음

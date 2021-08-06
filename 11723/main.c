009fa865da4f769deff92be1ccbfd8b6cffe2239
Hyeon Kim <simnalamburt@gmail.com>
2021-08-06 12:27:44 +0900

    11723: No sscanf()

--- 11723/main.c
+++ 11723/main.c
@@ -1,37 +1,41 @@
 #include <stdio.h>
 
-int main() {
-  char BUF[64];
+char BUF[64];
+int C;
 
-  int M;
-  fgets(BUF, sizeof BUF, stdin);
-  sscanf(BUF, "%d", &M);
+int scan_uint() {
+  int n = 0, c;
+  while ((c = BUF[C++]) >= '-') { n = 10*n + c - '0'; }
+  return n;
+}
+
+int main() {
+  fgets(BUF, sizeof BUF, stdin); C = 0;
+  int M = scan_uint();
 
   int bitset = 0;
   for (int _ = 0; _ < M; ++_) {
-    fgets(BUF, sizeof BUF, stdin);
-    int C = 0;
+    fgets(BUF, sizeof BUF, stdin); C = 0;
 
-    int x;
     if (BUF[C] == 'a' && BUF[C+1] == 'd') { // add
       C += 4;
+      int x = scan_uint();
 
-      sscanf(&BUF[C], "%d", &x);
       bitset |= (1 << x);
     } else if (BUF[C] == 'r') { // remove
       C += 7;
+      int x = scan_uint();
 
-      sscanf(&BUF[C], "%d", &x);
       bitset &= ~(1 << x);
     } else if (BUF[C] == 'c') { // check
       C += 6;
+      int x = scan_uint();
 
-      sscanf(&BUF[C], "%d", &x);
       printf("%d\n", !!(bitset & (1 << x)));
     } else if (BUF[C] == 't') { // toggle
       C += 7;
+      int x = scan_uint();
 
-      sscanf(&BUF[C], "%d", &x);
       bitset ^= (1 << x);
     } else if (BUF[C] == 'e') { // empty
       C += 6;

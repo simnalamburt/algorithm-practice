68a32ab7d327d1fc525fa97cdbb0b1b4cfd85ca7
Hyeon Kim <simnalamburt@gmail.com>
2021-08-11 19:05:44 +0900

    1697: AC

--- 1697/main.py
+++ 1697/main.py
@@ -1,3 +1,31 @@
+from heapq import heappush, heappop
+
 N, K = map(int, input().split())
 
-# NOTE: 다익스트라
+# Dijkstra's algorithm
+DIST = [float('inf')] * 100_001
+DIST[N] = 0
+Q = [(0, N)]
+while Q:
+    dist, pos = heappop(Q)
+
+    if DIST[pos] < dist:
+        continue
+
+    if pos == K:
+        break
+
+    for neighbor in [pos - 1, pos + 1, 2*pos]:
+        if not (0 <= neighbor < 100_001):
+            continue
+
+        new_dist = dist + 1
+        if DIST[neighbor] <= new_dist:
+            continue
+
+        DIST[neighbor] = new_dist
+        heappush(Q, (new_dist, neighbor))
+
+answer = DIST[K]
+
+print(answer)

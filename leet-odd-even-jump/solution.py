5fdd2ec81f547f64f858b358ab9277f453405cf3
Hyeon Kim <simnalamburt@gmail.com>
2021-08-12 01:48:16 +0900

    leet-odd-even-jump: Use sorted array

--- leet-odd-even-jump/solution.py
+++ leet-odd-even-jump/solution.py
@@ -1,18 +1,24 @@
+from bisect import bisect, insort
+
 class Solution:
     def oddEvenJumps(self, arr: list[int]) -> int:
         if not arr:
             return 0
 
-        DP: list[tuple[bool, bool]] = [None] * len(arr)
-        DP[-1] = (True, True)
+        DP: list[tuple[bool, bool]] = [None] * (len(arr) - 1) + [(True, True)]
+        sorted1: list[tuple[int, int]] = [(arr[-1], len(arr) - 1)]
+        sorted2: list[tuple[int, int]] = [(arr[-1], -(len(arr) - 1))]
 
         for i in reversed(range(len(arr) - 1)):
-            bigger = [j for j in range(i + 1, len(arr)) if arr[i] <= arr[j]]
-            smaller = [j for j in range(i + 1, len(arr)) if arr[i] >= arr[j]]
+            idx1 = bisect(sorted1, (arr[i], i))
+            idx2 = bisect(sorted2, (arr[i], -i))
 
             DP[i] = (
-                bigger and DP[min(bigger, key=lambda j:arr[j])][1],
-                smaller and DP[max(smaller, key=lambda j:arr[j])][0],
+                DP[sorted1[idx1][1]][1] if idx1 < len(sorted1) else False,
+                DP[-sorted2[idx2 - 1][1]][0] if idx2 > 0 else False,
             )
+
+            sorted1.insert(idx1, (arr[i], i)) # TODO: use BST
+            sorted2.insert(idx2, (arr[i], -i)) # TODO: use BST
 
         return sum(1 for is_odd_good, _ in DP if is_odd_good)

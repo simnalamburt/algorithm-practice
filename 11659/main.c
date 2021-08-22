e808902c64034508e487802f64ebeac0e1c720b4
Hyeon Kim <simnalamburt@gmail.com>
2021-08-22 21:41:16 +0900

    11659: 4등

--- 11659/main.c
+++ 11659/main.c
@@ -1,25 +1,24 @@
+#pragma GCC optimize("O4,unroll-loops")
+#pragma GCC target("arch=haswell")
 #include <unistd.h>
 
-char *BUF;
-int scan_uint() {
+static char BUF[1900014];
+static int scan_uint() {
   static int C = 0;
   int n = 0, c;
   while ((c = BUF[C++]) >= '-') { n = 10*n + c - '0'; }
   return n;
 }
 
-char *WBUF;
-int W = 0;
-void print_uint(int n) {
+static int W = 0;
+static void print_uint(int n) {
   if (n/10) { print_uint(n/10); }
-  WBUF[W++] = '0' + n%10;
+  BUF[W++] = '0' + n%10;
 }
 
 int main() { }
 int __libc_start_main() {
-  char buf[1900014];
-  BUF = WBUF = buf;
-  read(0, buf, sizeof buf);
+  read(0, BUF, sizeof BUF);
 
   int N = scan_uint(), M = scan_uint();
   int partialSums[N + 1];
@@ -30,14 +29,12 @@ int __libc_start_main() {
     partialSums[i + 1] = partialSum;
   }
   // Invariant: partialSums[i] = sum(nums[..i])
-  for (int i = 0; i < M; ++i) {
+  for (int _ = 0; _ < M; ++_) {
     int i = scan_uint(), j = scan_uint();
-    int start = i - 1, end = j;
-    int answer = partialSums[end] - partialSums[start];
-    print_uint(answer);
-    WBUF[W++] = '\n';
+    print_uint(partialSums[j] - partialSums[i - 1]);
+    BUF[W++] = '\n';
   }
 
-  write(1, WBUF, W);
+  write(1, BUF, W);
   _exit(0);
 }

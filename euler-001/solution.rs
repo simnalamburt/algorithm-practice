e12fa615f9ef31fa92f16ab1c595af1896fb2a75
Hyeon Kim <simnalamburt@gmail.com>
2021-12-19 00:39:49 +0900

    euler-001: Run rustfmt

--- euler-001/solution.rs
+++ euler-001/solution.rs
@@ -1,21 +1,30 @@
 use std::io::{self, Write};
 
 fn main() -> Result<(), io::Error> {
-  let default = 1000;
-  print!("Bound? (default: {})\n> ", default);
-  io::stdout().flush()?;
+    let default = 1000;
+    print!("Bound? (default: {})\n> ", default);
+    io::stdout().flush()?;
 
-  let mut line = String::new();
-  io::stdin().read_line(&mut line)?;
-  let bound: u64 = line.trim().parse().unwrap_or(default);
+    let mut line = String::new();
+    io::stdin().read_line(&mut line)?;
+    let bound: u64 = line.trim().parse().unwrap_or(default);
 
-  // sigma(1000) = 1 + 2 + 3 + ... + 1000
-  fn sigma(bound: u64) -> u64 { return bound*(bound + 1)/2; }
+    // sigma(1000) = 1 + 2 + 3 + ... + 1000
+    fn sigma(bound: u64) -> u64 {
+        return bound * (bound + 1) / 2;
+    }
 
-  // sum(3) = sum of { x | 0 <= x < 1000, x % 3 == 0 }
-  let sum = |mul: u64| -> u64 { return mul*sigma((bound - 1)/mul); };
+    // sum(3) = sum of { x | 0 <= x < 1000, x % 3 == 0 }
+    let sum = |mul: u64| -> u64 {
+        return mul * sigma((bound - 1) / mul);
+    };
 
-  println!("The answer is : {} + {} - {} = {}",
-      sum(3), sum(5), sum(3*5), sum(3) + sum(5) - sum(3*5));
-  Ok(())
+    println!(
+        "The answer is : {} + {} - {} = {}",
+        sum(3),
+        sum(5),
+        sum(3 * 5),
+        sum(3) + sum(5) - sum(3 * 5)
+    );
+    Ok(())
 }

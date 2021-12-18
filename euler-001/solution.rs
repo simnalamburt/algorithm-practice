6dbfcf792d9d31d8db5464811f85067387bc1edd
Hyeon Kim <simnalamburt@gmail.com>
2021-12-19 00:38:42 +0900

    euler-001: Rewrite with stable Rust 1.x
    Existing code was from Rust pre-1.0 era.

--- euler-001/solution.rs
+++ euler-001/solution.rs
@@ -1,18 +1,21 @@
-use std::io;
+use std::io::{self, Write};
 
-fn main() {
+fn main() -> Result<(), io::Error> {
   let default = 1000;
   print!("Bound? (default: {})\n> ", default);
+  io::stdout().flush()?;
 
-  let line = io::stdin().read_line().ok().unwrap();
-  let bound: uint = from_str(line.trim()).unwrap_or(default);
+  let mut line = String::new();
+  io::stdin().read_line(&mut line)?;
+  let bound: u64 = line.trim().parse().unwrap_or(default);
 
   // sigma(1000) = 1 + 2 + 3 + ... + 1000
-  fn sigma(bound: uint) -> uint { return bound*(bound + 1)/2; }
+  fn sigma(bound: u64) -> u64 { return bound*(bound + 1)/2; }
 
   // sum(3) = sum of { x | 0 <= x < 1000, x % 3 == 0 }
-  let sum = |mul: uint| -> uint { return mul*sigma((bound - 1)/mul); };
+  let sum = |mul: u64| -> u64 { return mul*sigma((bound - 1)/mul); };
 
   println!("The answer is : {} + {} - {} = {}",
       sum(3), sum(5), sum(3*5), sum(3) + sum(5) - sum(3*5));
+  Ok(())
 }
